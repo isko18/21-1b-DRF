a3f7a7d39c6eca9b72e2ff722a4d697c12c0a92f
isko18 <toksonbaevislam2004@gmail.com>
2025-02-06 18:33:12 +0600

    drf

--- apps/main/views.py
+++ apps/main/views.py
@@ -4,6 +4,9 @@ from apps.main.models import Settings, Products
 from apps.main.serializer import  ProductSerializer
 from apps.main.pagination import ProductPagination
 
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
 class ProductMixins(GenericViewSet,
                     mixins.CreateModelMixin,
                     mixins.ListModelMixin,
@@ -13,3 +16,10 @@ class ProductMixins(GenericViewSet,
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
     pagination_class = ProductPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    
+    search_fields = ['title', 'description']
+    ordering_fields = ['price', 'created_at']
+    ordering = ['-created_at']
+    
+    
